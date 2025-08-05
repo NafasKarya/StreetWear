1f5d0777f7de5a0ce6ee99f930fd99dfd6f3eac4
Lif Siregar <khalifsiregar123@gmail.com>
2025-08-06 00:13:51 +0700

    updaload

---
+++ global.d.ts
@@ -0,0 +1,20 @@
+import React from 'react';
+declare namespace JSX {
+  interface IntrinsicElements {
+    'model-viewer': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+      src?: string;
+      alt?: string;
+      'camera-controls'?: boolean;
+      'camera-orbit'?: string;
+      'auto-rotate'?: boolean;
+      rotation?: string;
+      style?: React.CSSProperties;
+      exposure?: string;
+      'interaction-prompt'?: string;
+      'background-color'?: string;
+      'disable-zoom'?: boolean;
+      'disable-pan'?: boolean;
+      [key: string]: any;
+    };
+  }
+}
